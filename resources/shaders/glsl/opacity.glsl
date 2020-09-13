d1adb313e62ba138c68515a9eadd7de13e6847d5
Leon Styhre <leon@leonstyhre.com>
2020-09-13 09:57:00 +0200

    Improved the opacity shader.

--- resources/shaders/glsl/opacity.glsl
+++ resources/shaders/glsl/opacity.glsl
@@ -28,9 +28,8 @@ varying vec2 vTexCoord;
 void main()
 {
     vec4 color = texture2D(myTexture, vTexCoord);
-    float alpha = clamp(color.a-(1-opacity), 0.0, 1.0);
 
-    gl_FragColor = vec4(color.rgb, alpha);
+    gl_FragColor = vec4(color.rgb, color.a * opacity);
 }
 
 #endif
