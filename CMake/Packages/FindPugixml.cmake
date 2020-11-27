cc93ca4630c1fddb4d5ddb54350c3cf1414b8a2f
Leon Styhre <leon@leonstyhre.com>
2020-11-27 20:04:02 +0100

    Fixed an annoying CMake issue that also broke the macOS build.

--- CMake/Packages/FindPugixml.cmake
+++ CMake/Packages/FindPugixml.cmake
@@ -11,32 +11,34 @@
 
 include(FindPkgMacros)
 
-if (NOT WIN32)
+if(NOT WIN32)
     find_package(PkgConfig)
     pkg_check_modules(PUGIXML REQUIRED pugixml>=1.09)
-endif (NOT WIN32)
+    # Set the full path to the library instead of just 'pugixml'.
+    set(PUGIXML_LIBRARIES ${PUGIXML_LINK_LIBRARIES})
+endif(NOT WIN32)
 
-if (WIN32)
+if(WIN32)
     find_path(PUGIXML_INCLUDE_DIR pugixml.hpp)
 
 # Support the REQUIRED and QUIET arguments, and set PUGIXML_FOUND if found.
-include (FindPackageHandleStandardArgs)
+include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(PUGIXML DEFAULT_MSG PUGIXML_INCLUDE_DIR)
 
-if (NOT PUGIXML_INCLUDE_DIR)
+if(NOT PUGIXML_INCLUDE_DIR)
     message(FATAL_ERROR "PUGIXML include files not found!")
 endif()
 
-endif (WIN32)
+endif(WIN32)
 
 find_library(PUGIXML_LIBRARY pugixml)
 
-if (NOT PUGIXML_LIBRARY)
+if(NOT PUGIXML_LIBRARY)
     message(FATAL_ERROR "libpugixml library not found!")
 endif()
 
 # Support the REQUIRED and QUIET arguments, and set PUGIXML_FOUND if found.
-include (FindPackageHandleStandardArgs)
+include(FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args(pugixml DEFAULT_MSG PUGIXML_LIBRARY)
+find_package_handle_standard_args(Pugixml DEFAULT_MSG PUGIXML_LIBRARY)
