62c5c379fa56ac21ad5e8d2e1d2cb6ab4fd47a83
Leon Styhre <leon@leonstyhre.com>
2021-07-17 10:10:14 +0200

    Bumped the version to v1.1.0-dev

--- es-app/src/EmulationStation.h
+++ es-app/src/EmulationStation.h
@@ -15,7 +15,7 @@
 #define PROGRAM_VERSION_MINOR        1
 #define PROGRAM_VERSION_MAINTENANCE  0
 // clang-format on
-#define PROGRAM_VERSION_STRING "1.1.0-rc"
+#define PROGRAM_VERSION_STRING "1.1.0-dev"
 
 #define PROGRAM_BUILT_STRING __DATE__ " - " __TIME__
 
