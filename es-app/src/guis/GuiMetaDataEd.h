5c4d0821e290f5c4d16cb28bed7ebc8451ab6bc5
Leon Styhre <leon@leonstyhre.com>
2021-10-23 22:49:34 +0200

    Renamed 'Controller' to 'Controller badge' in the metadata editor.

--- es-app/src/guis/GuiMetaDataEd.h
+++ es-app/src/guis/GuiMetaDataEd.h
@@ -61,7 +61,7 @@ private:
 
     ScraperSearchParams mScraperParams;
 
-    std::vector<GameControllers> mGameControllers;
+    std::vector<GameControllers> mControllerBadges;
     std::vector<std::shared_ptr<GuiComponent>> mEditors;
 
     std::vector<MetaDataDecl> mMetaDataDecl;
