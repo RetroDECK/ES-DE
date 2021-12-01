a2253113f7c3a42965b5c53621c5ea5ba4c22b8d
Leon Styhre <leon@leonstyhre.com>
2021-12-01 17:57:23 +0100

    (macOS) Some fixes to make the legacy build work again.

--- es-core/src/ThemeData.h
+++ es-core/src/ThemeData.h
@@ -127,6 +127,21 @@ public:
 
         template <typename T> const T get(const std::string& prop) const
         {
+#if defined(LEGACY_MACOS)
+            if (std::is_same<T, glm::vec2>::value)
+                return *(const T*)&properties.at(prop).v;
+            else if (std::is_same<T, std::string>::value)
+                return *(const T*)&properties.at(prop).s;
+            else if (std::is_same<T, unsigned int>::value)
+                return *(const T*)&properties.at(prop).i;
+            else if (std::is_same<T, float>::value)
+                return *(const T*)&properties.at(prop).f;
+            else if (std::is_same<T, bool>::value)
+                return *(const T*)&properties.at(prop).b;
+            else if (std::is_same<T, glm::vec4>::value)
+                return *(const T*)&properties.at(prop).r;
+            return T();
+#else
             if (std::is_same<T, glm::vec2>::value)
                 return std::any_cast<const T>(properties.at(prop).v);
             else if (std::is_same<T, std::string>::value)
@@ -140,6 +155,7 @@ public:
             else if (std::is_same<T, glm::vec4>::value)
                 return std::any_cast<const T>(properties.at(prop).r);
             return T();
+#endif
         }
 
         bool has(const std::string& prop) const
