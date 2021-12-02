ae0605b78502bd00f26becb153b6d85dcdb7da94
Leon Styhre <leon@leonstyhre.com>
2021-12-02 17:59:39 +0100

    Bumped the version to 1.2.0-beta2

--- es-app/src/EmulationStation.h
+++ es-app/src/EmulationStation.h
@@ -15,7 +15,7 @@
 #define PROGRAM_VERSION_MINOR        2
 #define PROGRAM_VERSION_MAINTENANCE  0
 // clang-format on
-#define PROGRAM_VERSION_STRING "1.2.0-beta2-dev"
+#define PROGRAM_VERSION_STRING "1.2.0-beta2"
 
 #define PROGRAM_BUILT_STRING __DATE__ " - " __TIME__
 
