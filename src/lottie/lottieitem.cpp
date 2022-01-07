79ff784721f4467450f37310322ecff59d3f1ce9
Leon Styhre <leon@leonstyhre.com>
2022-01-07 18:43:06 +0100

    Squashed 'external/rlottie/' changes from bf3d272df..875626965
    875626965 Check border of color table while generating gradient
    d1636c7b4 Ignore animations with objects of unspecified type
    f3eed9a33 add formal parameter void to lottie_init() and lottie_shutdown()
    519bd08b0 Add lottie_init() and lottie_shutdown() c api.
    447cb7dd1 replace unsigned int with uint32_t in Int()
    d7cf0a5ab replace uint ushort and uchar typedefs with uint32 uint16 and uint8 -- added format script to run clang format on latest commit.
    16beec845 lottiemodel: Improve opacity calculation for gradient stop
    327fb7dba Reject reversed frames
    625bc4c48 Finite loop in VBezier::tAtLength()
    ddbc9dce7 rlottie/capi: Add missing multiply for Path
    c0e16e571 Check Layer.mExtra and Transform.mExtra before dereferencing
    9f78532c8 Avoid access to last element of empty mChildren vector
    4e08acd8c remove RAPIDJSON_ASSERT() by placing the parser in error state
    7affa78fe Fix dereferencing of null pointer in model::Layer::solidColor() getter
    9d17ff8e6 Skip dash array of size one or zero
    f379dfe15 add the /inc folder as an include path at build time
    194b31736 Improve matte rendering performance for simple layer
    7c5b40cbb CMake: fix MSVC warnings
    fa44b753e fix compileing error
    41f906480 On windows, stbi__fopen opens a path containing non-English characters or symbols will fail due to encoding problems, so the filename parameter is assumed to be utf-8 and converted to utf-16, and then call _wfopen_s or _wfopen
    29b391b95 add lottie_configure_model_cache_size() c api
    3cd00151c Fix comparison
    bd4c4e1f8 MT and MTd
    c593b57eb Remove link warnings on MSVC
    132ca0a00 #include "fix" for rust-bindgen
    c9780b46d Fix CMake build in subdirectory
    1cb2021d6 Fix crash when path animation  data is empty
    ad9beaec6 Added support for RoundedCornor objects.
    a41bd7383 rlottie: draw line if width or height is 0
    ed94baf14 resource: polish player png image.
    9b4c4d2af resource: add rlottie_player image.
    2d7b1fa2b Fixed build with GCC 11.
    9cd9a6e95 vs2019: Add release build option
    d733e953b updated AUTHORS
    fc760ef92 updated AUTHORS
    d92d4aaf6 wasm: Add extra functions for lottie animation to use them in rlottie viewer
    09662b89d updated AUTHORS
    71fbff5d5 example/win32Player: removed unnecessary files.
    d25911e6c add app executables
    538dfd9aa rename directory rlottiePlayer to win32Player
    6fbb67d49 fix openJSONFileDialog
    85856dff3 fix animation replace error
    f86e97d65 fix animation bitmap size to be set dynamically
    55815b2cb move function prototype to animation.h To prevent function prototype duplicate code in source.cpp.
    9a71eeca9 add an example of win32 app that play rlottie animation by vs2019
    619320cbb add rlottiePlayer Project
    4884e0e9b Modify: Update .gitignore
    3ebf0c7e3 Docs: Fix README typo
    502cc3638 updated license in spec file.
    e812ba9e9 Docs: Fix README typo
    c98ec565d Update README.md
    33295c22f Fixed README typo
    073224e69 fixed readme typo
    ff8ddfc49 ci: upgrade windows build chain to VS2017
    10c1055a3 fixed sign extension warning
    98ed2620a build: use shared_module instead of shared_library
    
    git-subtree-dir: external/rlottie
    git-subtree-split: 875626965959d8e269ca22175c8e1ad190696c43

--- src/lottie/lottieitem.cpp
+++ src/lottie/lottieitem.cpp
@@ -149,9 +149,9 @@ bool renderer::Composition::update(int frameNo, const VSize &size,
 
 bool renderer::Composition::render(const rlottie::Surface &surface)
 {
-    mSurface.reset(reinterpret_cast<uchar *>(surface.buffer()),
-                   uint(surface.width()), uint(surface.height()),
-                   uint(surface.bytesPerLine()),
+    mSurface.reset(reinterpret_cast<uint8_t *>(surface.buffer()),
+                   uint32_t(surface.width()), uint32_t(surface.height()),
+                   uint32_t(surface.bytesPerLine()),
                    VBitmap::Format::ARGB32_Premultiplied);
 
     /* schedule all preprocess task for this frame at once.
@@ -200,7 +200,7 @@ VRle renderer::Mask::rle()
 {
     if (!vCompare(mCombinedAlpha, 1.0f)) {
         VRle obj = mRasterizer.rle();
-        obj *= uchar(mCombinedAlpha * 255);
+        obj *= uint8_t(mCombinedAlpha * 255);
         return obj;
     } else {
         return mRasterizer.rle();
@@ -343,7 +343,7 @@ renderer::Layer::Layer(model::Layer *layerData) : mLayerData(layerData)
         mLayerMask = std::make_unique<renderer::LayerMask>(mLayerData);
 }
 
-bool renderer::Layer::resolveKeyPath(LOTKeyPath &keyPath, uint depth,
+bool renderer::Layer::resolveKeyPath(LOTKeyPath &keyPath, uint32_t depth,
                                      LOTVariant &value)
 {
     if (!keyPath.matches(name(), depth)) {
@@ -359,12 +359,12 @@ bool renderer::Layer::resolveKeyPath(LOTKeyPath &keyPath, uint depth,
     return true;
 }
 
-bool renderer::ShapeLayer::resolveKeyPath(LOTKeyPath &keyPath, uint depth,
+bool renderer::ShapeLayer::resolveKeyPath(LOTKeyPath &keyPath, uint32_t depth,
                                           LOTVariant &value)
 {
     if (renderer::Layer::resolveKeyPath(keyPath, depth, value)) {
         if (keyPath.propagate(name(), depth)) {
-            uint newDepth = keyPath.nextDepth(name(), depth);
+            uint32_t newDepth = keyPath.nextDepth(name(), depth);
             mRoot->resolveKeyPath(keyPath, newDepth, value);
         }
         return true;
@@ -372,12 +372,12 @@ bool renderer::ShapeLayer::resolveKeyPath(LOTKeyPath &keyPath, uint depth,
     return false;
 }
 
-bool renderer::CompLayer::resolveKeyPath(LOTKeyPath &keyPath, uint depth,
+bool renderer::CompLayer::resolveKeyPath(LOTKeyPath &keyPath, uint32_t depth,
                                          LOTVariant &value)
 {
     if (renderer::Layer::resolveKeyPath(keyPath, depth, value)) {
         if (keyPath.propagate(name(), depth)) {
-            uint newDepth = keyPath.nextDepth(name(), depth);
+            uint32_t newDepth = keyPath.nextDepth(name(), depth);
             for (const auto &layer : mLayers) {
                 layer->resolveKeyPath(keyPath, newDepth, value);
             }
@@ -507,7 +507,7 @@ void renderer::CompLayer::render(VPainter *painter, const VRle &inheritMask,
             renderHelper(&srcPainter, inheritMask, matteRle, cache);
             srcPainter.end();
             painter->drawBitmap(VPoint(), srcBitmap,
-                                uchar(combinedAlpha() * 255.0f));
+                                uint8_t(combinedAlpha() * 255.0f));
             cache.release_surface(srcBitmap);
         } else {
             renderHelper(painter, inheritMask, matteRle, cache);
@@ -597,11 +597,21 @@ void renderer::CompLayer::renderMatteLayer(VPainter *painter, const VRle &mask,
         srcBitmap.updateLuma();
     }
 
+    auto clip = layerPainter.clipBoundingRect();
+
+    // if the layer has only one renderer then use it as the clip rect
+    // when blending 2 buffer and copy back to final buffer to avoid
+    // unnecessary pixel processing.
+    if (layer->renderList().size() == 1)
+    {
+        clip = layer->renderList()[0]->rle().boundingRect();
+    }
+
     // 2.3 draw src buffer as mask
-    layerPainter.drawBitmap(VPoint(), srcBitmap);
+    layerPainter.drawBitmap(clip, srcBitmap, clip);
     layerPainter.end();
     // 3. draw the result buffer into painter
-    painter->drawBitmap(VPoint(), layerBitmap);
+    painter->drawBitmap(clip, layerBitmap, clip);
 
     cache.release_surface(srcBitmap);
     cache.release_surface(layerBitmap);
@@ -853,7 +863,7 @@ renderer::DrawableList renderer::ShapeLayer::renderList()
     return {mDrawableList.data(), mDrawableList.size()};
 }
 
-bool renderer::Group::resolveKeyPath(LOTKeyPath &keyPath, uint depth,
+bool renderer::Group::resolveKeyPath(LOTKeyPath &keyPath, uint32_t depth,
                                      LOTVariant &value)
 {
     if (!keyPath.skip(name())) {
@@ -870,7 +880,7 @@ bool renderer::Group::resolveKeyPath(LOTKeyPath &keyPath, uint depth,
     }
 
     if (keyPath.propagate(name(), depth)) {
-        uint newDepth = keyPath.nextDepth(name(), depth);
+        uint32_t newDepth = keyPath.nextDepth(name(), depth);
         for (auto &child : mContents) {
             child->resolveKeyPath(keyPath, newDepth, value);
         }
@@ -878,7 +888,7 @@ bool renderer::Group::resolveKeyPath(LOTKeyPath &keyPath, uint depth,
     return true;
 }
 
-bool renderer::Fill::resolveKeyPath(LOTKeyPath &keyPath, uint depth,
+bool renderer::Fill::resolveKeyPath(LOTKeyPath &keyPath, uint32_t depth,
                                     LOTVariant &value)
 {
     if (!keyPath.matches(mModel.name(), depth)) {
@@ -893,7 +903,7 @@ bool renderer::Fill::resolveKeyPath(LOTKeyPath &keyPath, uint depth,
     return false;
 }
 
-bool renderer::Stroke::resolveKeyPath(LOTKeyPath &keyPath, uint depth,
+bool renderer::Stroke::resolveKeyPath(LOTKeyPath &keyPath, uint32_t depth,
                                       LOTVariant &value)
 {
     if (!keyPath.matches(mModel.name(), depth)) {
@@ -1097,7 +1107,7 @@ void renderer::Rect::updatePath(VPath &path, int frameNo)
 {
     VPointF pos = mData->mPos.value(frameNo);
     VPointF size = mData->mSize.value(frameNo);
-    float   roundness = mData->mRound.value(frameNo);
+    float   roundness = mData->roundness(frameNo);
     VRectF  r(pos.x() - size.x() / 2, pos.y() - size.y() / 2, size.x(),
              size.y());
 
--- src/lottie/lottiemodel.h
+++ src/lottie/lottiemodel.h
@@ -57,9 +57,9 @@ inline T lerp(const T &start, const T &end, float t)
 
 namespace model {
 
-enum class MatteType : uchar { None = 0, Alpha = 1, AlphaInv, Luma, LumaInv };
+enum class MatteType : uint8_t { None = 0, Alpha = 1, AlphaInv, Luma, LumaInv };
 
-enum class BlendMode : uchar {
+enum class BlendMode : uint8_t {
     Normal = 0,
     Multiply = 1,
     Screen = 2,
@@ -72,8 +72,8 @@ public:
     Color(float red, float green, float blue) : r(red), g(green), b(blue) {}
     VColor toColor(float a = 1)
     {
-        return VColor(uchar(255 * r), uchar(255 * g), uchar(255 * b),
-                      uchar(255 * a));
+        return VColor(uint8_t(255 * r), uint8_t(255 * g), uint8_t(255 * b),
+                      uint8_t(255 * a));
     }
     friend inline Color operator+(const Color &c1, const Color &c2);
     friend inline Color operator-(const Color &c1, const Color &c2);
@@ -111,6 +111,11 @@ struct PathData {
                      VPath &result)
     {
         result.reset();
+        // test for empty animation data.
+        if (start.mPoints.empty() || end.mPoints.empty())
+        {
+            return;
+        }
         auto size = std::min(start.mPoints.size(), end.mPoints.size());
         /* reserve exact memory requirement at once
          * ptSize = size + 1(size + close)
@@ -440,7 +445,8 @@ public:
         Path,
         Polystar,
         Trim,
-        Repeater
+        Repeater,
+        RoundedCorner
     };
 
     explicit Object(Object::Type type) : mPtr(nullptr)
@@ -661,7 +667,7 @@ public:
 
 class Layer : public Group {
 public:
-    enum class Type : uchar {
+    enum class Type : uint8_t {
         Precomp = 0,
         Solid = 1,
         Image = 2,
@@ -670,6 +676,7 @@ public:
         Text = 5
     };
     Layer() : Group(Object::Type::Layer) {}
+    bool    hasRoundedCorner() const noexcept { return mHasRoundedCorner; }
     bool    hasPathOperator() const noexcept { return mHasPathOperator; }
     bool    hasGradient() const noexcept { return mHasGradient; }
     bool    hasMask() const noexcept { return mHasMask; }
@@ -680,7 +687,10 @@ public:
     int     inFrame() const noexcept { return mInFrame; }
     int     outFrame() const noexcept { return mOutFrame; }
     int     startFrame() const noexcept { return mStartFrame; }
-    Color   solidColor() const noexcept { return mExtra->mSolidColor; }
+    Color   solidColor() const noexcept
+    {
+        return mExtra ? mExtra->mSolidColor : Color();
+    }
     bool    autoOrient() const noexcept { return mAutoOrient; }
     int     timeRemap(int frameNo) const;
     VSize   layerSize() const { return mLayerSize; }
@@ -694,10 +704,7 @@ public:
     {
         return mTransform ? mTransform->opacity(frameNo) : 1.0f;
     }
-    Asset *asset() const
-    {
-        return (mExtra && mExtra->mAsset) ? mExtra->mAsset : nullptr;
-    }
+    Asset *asset() const { return mExtra ? mExtra->mAsset : nullptr; }
     struct Extra {
         Color               mSolidColor;
         std::string         mPreCompRefId;
@@ -717,6 +724,7 @@ public:
     MatteType mMatteType{MatteType::None};
     Type      mLayerType{Layer::Type::Null};
     BlendMode mBlendMode{BlendMode::Normal};
+    bool      mHasRoundedCorner{false};
     bool      mHasPathOperator{false};
     bool      mHasMask{false};
     bool      mHasRepeater{false};
@@ -813,6 +821,7 @@ public:
 
 private:
     void populate(VGradientStops &stops, int frameNo);
+    float getOpacityAtPosition(float *opacities, size_t opacityArraySize, float position);
 
 public:
     int                      mGradientType{1};    /* "t" Linear=1 , Radial = 2*/
@@ -893,11 +902,30 @@ public:
     Property<PathData> mShape;
 };
 
+class RoundedCorner : public Object {
+public:
+    RoundedCorner() : Object(Object::Type::RoundedCorner) {}
+    float radius(int frameNo) const { return mRadius.value(frameNo);}
+public:
+    Property<float>   mRadius{0};
+};
+
 class Rect : public Shape {
 public:
     Rect() : Shape(Object::Type::Rect) {}
+    float roundness(int frameNo)
+    {
+        return mRoundedCorner ? mRoundedCorner->radius(frameNo) :
+                                mRound.value(frameNo);
+    }
 
+    bool roundnessChanged(int prevFrame, int curFrame)
+    {
+        return mRoundedCorner ? mRoundedCorner->mRadius.changed(prevFrame, curFrame) :
+                        mRound.changed(prevFrame, curFrame);
+    }
 public:
+    RoundedCorner*    mRoundedCorner{nullptr};
     Property<VPointF> mPos;
     Property<VPointF> mSize;
     Property<float>   mRound{0};
