afe249c5fc3bdc2e57ac8afac9ccea57cb4d22bb
Leon Styhre <leon@leonstyhre.com>
2022-02-04 21:42:08 +0100

    Added CarouselComponent skeleton.

---
+++ es-core/src/components/CarouselComponent.cpp
@@ -0,0 +1,14 @@
+//  SPDX-License-Identifier: MIT
+//
+//  EmulationStation Desktop Edition
+//  CarouselComponent.cpp
+//
+//  Carousel.
+//
+
+#include "components/CarouselComponent.h"
+
+CarouselComponent::CarouselComponent()
+{
+    //
+}
---
+++ es-core/src/components/CarouselComponent.h
@@ -0,0 +1,22 @@
+//  SPDX-License-Identifier: MIT
+//
+//  EmulationStation Desktop Edition
+//  CarouselComponent.h
+//
+//  Carousel.
+//
+
+#include "GuiComponent.h"
+
+#ifndef ES_CORE_COMPONENTS_CAROUSEL_COMPONENT_H
+#define ES_CORE_COMPONENTS_CAROUSEL_COMPONENT_H
+
+class CarouselComponent : public GuiComponent
+{
+public:
+    CarouselComponent();
+
+private:
+};
+
+#endif // ES_CORE_COMPONENTS_CAROUSEL_COMPONENT_H
