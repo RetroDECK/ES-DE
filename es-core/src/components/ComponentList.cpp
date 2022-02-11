ead1d5af9ce73b1c2a897d5c93a0b00ecaf4ab95
Leon Styhre <leon@leonstyhre.com>
2022-02-11 23:33:29 +0100

    Fixed a small alignment issue in ComponentList.

--- es-core/src/components/ComponentList.cpp
+++ es-core/src/components/ComponentList.cpp
@@ -289,7 +289,7 @@ void ComponentList::render(const glm::mat4& parentTrans)
     dim.x = (trans[0].x * dim.x + trans[3].x) - trans[3].x;
     dim.y = (trans[1].y * dim.y + trans[3].y) - trans[3].y;
 
-    const int clipRectPosX {static_cast<int>(std::ceil(trans[3].x))};
+    const int clipRectPosX {static_cast<int>(std::round(trans[3].x))};
     const int clipRectPosY {static_cast<int>(std::round(trans[3].y))};
     const int clipRectSizeX {static_cast<int>(std::round(dim.x))};
     const int clipRectSizeY {static_cast<int>(std::round(dim.y))};
