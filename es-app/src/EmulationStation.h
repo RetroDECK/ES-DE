b9f5b03005335db208df3436fc01963dec4d14ba
Leon Styhre <leon@leonstyhre.com>
2022-02-20 20:00:51 +0100

    Changed the version from 1.3.0-alpha to 2.0.0-alpha

--- es-app/src/EmulationStation.h
+++ es-app/src/EmulationStation.h
@@ -11,15 +11,15 @@
 // These numbers and strings need to be manually updated for a new version.
 // Do this version number update as the very last commit for the new release version.
 // clang-format off
-#define PROGRAM_VERSION_MAJOR        1
-#define PROGRAM_VERSION_MINOR        3
+#define PROGRAM_VERSION_MAJOR        2
+#define PROGRAM_VERSION_MINOR        0
 #define PROGRAM_VERSION_MAINTENANCE  0
 // clang-format on
-#define PROGRAM_VERSION_STRING "1.3.0-alpha"
+#define PROGRAM_VERSION_STRING "2.0.0-alpha"
 
 #define PROGRAM_BUILT_STRING __DATE__ " - " __TIME__
 
-#define RESOURCE_VERSION_STRING "1,3,0\0"
+#define RESOURCE_VERSION_STRING "2,0,0\0"
 #define RESOURCE_VERSION PROGRAM_VERSION_MAJOR, PROGRAM_VERSION_MINOR, PROGRAM_VERSION_MAINTENANCE
 
 #endif // ES_APP_EMULATION_STATION_H
