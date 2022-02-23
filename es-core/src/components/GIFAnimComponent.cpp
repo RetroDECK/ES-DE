abb48e45e7d3b839c6bec920e85e941587ef7158
Leon Styhre <leon@leonstyhre.com>
2022-02-23 22:04:18 +0100

    Added GIFAnimComponent skeleton.

---
+++ es-core/src/components/GIFAnimComponent.cpp
@@ -0,0 +1,19 @@
+//  SPDX-License-Identifier: MIT
+//
+//  EmulationStation Desktop Edition
+//  GIFAnimComponent.cpp
+//
+//  Component to play GIF animations.
+//
+
+#include "components/GIFAnimComponent.h"
+
+GIFAnimComponent::GIFAnimComponent()
+{
+    //
+}
+
+GIFAnimComponent::~GIFAnimComponent()
+{
+    //
+}
---
+++ es-core/src/components/GIFAnimComponent.h
@@ -0,0 +1,23 @@
+//  SPDX-License-Identifier: MIT
+//
+//  EmulationStation Desktop Edition
+//  GIFAnimComponent.h
+//
+//  Component to play GIF animations.
+//
+
+#ifndef ES_CORE_COMPONENTS_GIF_ANIM_COMPONENT_H
+#define ES_CORE_COMPONENTS_GIF_ANIM_COMPONENT_H
+
+#include "GuiComponent.h"
+
+class GIFAnimComponent : public GuiComponent
+{
+public:
+    GIFAnimComponent();
+    ~GIFAnimComponent();
+
+private:
+};
+
+#endif // ES_CORE_COMPONENTS_GIF_ANIM_COMPONENT_H
