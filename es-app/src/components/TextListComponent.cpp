07f151d906d3a760436c614ccee594539b32a614
Leon Styhre <leon@leonstyhre.com>
2022-03-19 09:55:05 +0100

    Moved TextListComponent and CarouselComponent from es-core to es-app.

---
+++ es-app/src/components/TextListComponent.cpp
@@ -0,0 +1,9 @@
+//  SPDX-License-Identifier: MIT
+//
+//  EmulationStation Desktop Edition
+//  TextListComponent.cpp
+//
+//  Text list used for displaying and navigating the gamelist views.
+//
+
+#include "components/TextListComponent.h"
