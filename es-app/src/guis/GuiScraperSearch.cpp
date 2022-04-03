4a511f3f4a5db96f00aead077ff352ce3302bbcb
Leon Styhre <leon@leonstyhre.com>
2022-04-03 17:32:09 +0200

    Fixed an issue where the help system would not update properly during scraping.

--- es-app/src/guis/GuiScraperSearch.cpp
+++ es-app/src/guis/GuiScraperSearch.cpp
@@ -439,6 +439,7 @@ void GuiScraperSearch::onSearchDone(std::vector<ScraperSearchResult>& results)
 
     mBlockAccept = false;
     updateInfoPane();
+    updateHelpPrompts();
 
     // If there is a single result in semi-automatic mode or a single or more results in
     // fully automatic mode, then block the ability to manually accept the entry as it will
