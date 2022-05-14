b2f70971ef5c225b3d98fe352bf0e7424db6bcd7
Leon Styhre <leon@leonstyhre.com>
2022-05-14 11:01:01 +0200

    Changed the ScreenScraper system ID from pc to moonlight for the steam and epic systems.

--- es-app/src/scrapers/ScreenScraper.cpp
+++ es-app/src/scrapers/ScreenScraper.cpp
@@ -89,7 +89,7 @@ namespace
         {BANDAI_SUFAMI_TURBO, 108},
         {DOS, 135},
         {PC, 135},
-        {VALVE_STEAM, 135},
+        {VALVE_STEAM, 138},
         {NEC_PCFX, 72},
         {GAMEENGINE_PICO8, 234},
         {PHILIPS_CDI, 133},
