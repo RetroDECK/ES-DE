0dfdf1c20cf8812f050319db21b3108fec541196
Leon Styhre <leon@leonstyhre.com>
2022-08-14 21:31:59 +0200

    Fixed an issue where reloading using Ctrl+r would sometimes not update modified image files.

--- es-app/src/views/SystemView.cpp
+++ es-app/src/views/SystemView.cpp
@@ -94,6 +94,7 @@ bool SystemView::input(InputConfig* config, Input input)
         if (config->getDeviceId() == DEVICE_KEYBOARD && input.value && input.id == SDLK_r &&
             SDL_GetModState() & KMOD_LCTRL && Settings::getInstance()->getBool("Debug")) {
             LOG(LogDebug) << "SystemView::input(): Reloading all";
+            TextureResource::manualUnloadAll();
             ViewController::getInstance()->reloadAll();
             return true;
         }
