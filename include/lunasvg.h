2c16a78282ae7ea0dd62a90124ba162fb51c0d7a
Leon Styhre <leon@leonstyhre.com>
2023-05-08 19:53:29 +0200

    Squashed 'external/lunasvg/' changes from 585d61eef..f924651b8
    f924651b8 Merge pull request #129 from lemirep/master
    db27b7c0e Add missing cstdint include
    b66e22402 Merge pull request #126 from m-carrasco/fix-null-ptr-arithmetic
    a1edbff60 Merge pull request #125 from m-carrasco/fix-ub-memcpy
    f595f7be3 Fix UB caused by null pointer arithmetic.
    a854444e5 Fix UB caused by wrong call to memcpy.
    b7e72fb37 Merge pull request #84 from seanharmer/master
    4c16abf41 Merge pull request #123 from m-carrasco/fix-gradient-ub
    4d51541c2 Fix undefined behavior caused by a domain error on an sqrt call.
    d4a7080cf Merge pull request #1 from seanharmer/cmake_modernization
    b08d7d03a Relax the cmake version and other improvements
    33a458302 Modernize the cmake usage
    4b2609c14 Add initial .gitignore
    
    git-subtree-dir: external/lunasvg
    git-subtree-split: f924651b85cac47dbe15f51a4aa320461fc1d07b

--- include/lunasvg.h
+++ include/lunasvg.h
@@ -25,16 +25,10 @@
 
 #include <memory>
 #include <string>
+#include <cstdint>
 
-#if defined(_MSC_VER) && defined(LUNASVG_SHARED)
-#ifdef LUNASVG_EXPORT
-#define LUNASVG_API __declspec(dllexport)
-#else
-#define LUNASVG_API __declspec(dllimport)
-#endif
-#else
-#define LUNASVG_API
-#endif
+#include <lunasvg_export.h>
+#define LUNASVG_API LUNASVG_EXPORT
 
 namespace lunasvg {
 
