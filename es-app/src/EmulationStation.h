5eb0207b0f7edbd3e3653b86f5e1e3d65ae1685f
Leon Styhre <leon@leonstyhre.com>
2023-07-13 16:33:48 +0200

    Bumped the version to 2.1.0

--- es-app/src/EmulationStation.h
+++ es-app/src/EmulationStation.h
@@ -14,9 +14,9 @@
 #define PROGRAM_VERSION_MAJOR        2
 #define PROGRAM_VERSION_MINOR        1
 #define PROGRAM_VERSION_MAINTENANCE  0
-#define PROGRAM_RELEASE_NUMBER       34
+#define PROGRAM_RELEASE_NUMBER       35
 // clang-format on
-#define PROGRAM_VERSION_STRING "2.1.0-beta"
+#define PROGRAM_VERSION_STRING "2.1.0"
 
 #define PROGRAM_BUILT_STRING __DATE__ " - " __TIME__
 
