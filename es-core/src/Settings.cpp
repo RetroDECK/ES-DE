c97555a4c63bb547f344fa9deaf33dfa3944c84b
Leon Styhre <leon@leonstyhre.com>
2023-10-16 19:39:45 +0200

    Changed the default value for the ScraperSearchFileHashMaxSize setting from 192 to 384 MiB

--- es-core/src/Settings.cpp
+++ es-core/src/Settings.cpp
@@ -143,7 +143,7 @@ void Settings::setDefaults()
     mStringMap["ScraperLanguage"] = {"en", "en"};
     mIntMap["ScraperRetryOnErrorCount"] = {3, 3};
     mIntMap["ScraperRetryOnErrorTimer"] = {3, 3};
-    mIntMap["ScraperSearchFileHashMaxSize"] = {192, 192};
+    mIntMap["ScraperSearchFileHashMaxSize"] = {384, 384};
     mBoolMap["ScraperOverwriteData"] = {true, true};
     mBoolMap["ScraperHaltOnInvalidMedia"] = {true, true};
     mBoolMap["ScraperSearchFileHash"] = {true, true};
