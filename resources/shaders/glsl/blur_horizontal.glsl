1dbcba90e629334ea4b38f42a120cf2aff6bfdd7
Leon Styhre <leon@leonstyhre.com>
2023-12-26 11:10:19 +0100

    Changed the application name to ES-DE in the GLSL shader headers

--- resources/shaders/glsl/blur_horizontal.glsl
+++ resources/shaders/glsl/blur_horizontal.glsl
@@ -1,6 +1,6 @@
 //  SPDX-License-Identifier: MIT
 //
-//  EmulationStation Desktop Edition
+//  ES-DE
 //  blur_horizontal.glsl
 //
 //  Horizontal gaussian blur.
--- resources/shaders/glsl/blur_vertical.glsl
+++ resources/shaders/glsl/blur_vertical.glsl
@@ -1,6 +1,6 @@
 //  SPDX-License-Identifier: MIT
 //
-//  EmulationStation Desktop Edition
+//  ES-DE
 //  blur_vertical.glsl
 //
 //  Vertical gaussian blur.
