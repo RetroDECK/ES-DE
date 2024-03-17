66c827fffc12c19c00907a9f42b6cdb460353daf
XargonWan <XargonWan@gmail.com>
2024-03-17 10:24:27 +0100

    Merger script: added warning message

--- update_from_upstream.sh
+++ update_from_upstream.sh
@@ -2,3 +2,4 @@
 
 git fetch https://gitlab.com/es-de/emulationstation-de/ master  # Fetch the latest changes from the remote master branch
 git merge FETCH_HEAD  # Merge the fetched changes into your current branch
+echo -e "PLEASE CHECK IF ANYTHING IS CHANGED IN:\n-resources/systems/linux/es_find_rules.xml\n-resources/systems/linux/es_systems.xml"
