0e7083983bf13021ba09f59fee51fda77d7f2f0e
Leon Styhre <leon@leonstyhre.com>
2024-04-21 13:48:27 +0200

    (modern-es-de) Removed an unnecessary conversion script

--- themes/modern-es-de/art_legacy/convert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-EXTENSION=jpg
-
-for file in $(ls *.${EXTENSION}); do
-  echo convert $file $(basename -s $EXTENSION $file)webp
-  convert $file $(basename -s $EXTENSION $file)webp
-done
