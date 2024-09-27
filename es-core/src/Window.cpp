8dfd82f4cf8f8bb3cc35065cbd152adb79c3dcf2
XargonWan <XargonWan@gmail.com>
2024-09-27 12:46:33 +0900

    Ifdef, broken else

--- es-core/src/Window.cpp
+++ es-core/src/Window.cpp
@@ -173,7 +173,7 @@ bool Window::init()
     progressBarRect.barPosY += borderThickness;
     #if defined(__RETRODECK__)
         progressBarRect.color = 0xC858E6FF;
-    else
+    #else
         progressBarRect.color = 0x79010FFF;
     #endif
     mProgressBarRectangles.emplace_back(progressBarRect);
