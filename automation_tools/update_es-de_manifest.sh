cba639024ccfcf3c0ddaf5e16512f861c24f624d
XargonWan <XargonWan@gmail.com>
2024-09-27 10:09:16 +0900

    Updated build-locally (step 5) [skip ci]

--- automation_tools/update_es-de_manifest.sh
+++ automation_tools/update_es-de_manifest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/XargonWan/RetroDECK --depth=1 RetroDECK
+git clone https://github.com/RetroDECK/RetroDECK --depth=1 RetroDECK
 
 # Creating ES-DE manifest
 manifest_header="manifest-header.yml"
@@ -9,10 +9,10 @@ esde_manifest="net.retrodeck.es-de.yml"
 command="/app/bin/es-de" 
 
 # sed -n '/command/q;p' RetroDECK/net.retrodeck.retrodeck.yml > "$manifest_header"  TEMPORARY DISABLED TO TRY A BUILD WITH ANOTHER RUNTIME
-echo -e "command: $command\n" >> "$manifest_header"
+# echo -e "command: $command\n" >> "$manifest_header"
 sed -i '/^[[:space:]]*#/d' "$manifest_header"
 sed -i 's/[[:space:]]*#.*$//' "$manifest_header"
-sed -n '/finish-args:/,${/cleanup:/q;p;}' RetroDECK/net.retrodeck.es-de.yml >> "$manifest_header"
+sed -n '/finish-args:/,${/cleanup:/q;p;}' RetroDECK/net.retrodeck.retrodeck.yml >> "$manifest_header"
 
 sed -i 's/net.retrodeck.retrodeck/net.retrodeck.es-de/' "$manifest_header"
 
