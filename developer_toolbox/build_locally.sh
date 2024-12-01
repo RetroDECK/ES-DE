44fb412e3d485a2a403912b0f7d48d3b46fbef6c
XargonWan <XargonWan@gmail.com>
2024-12-01 22:46:56 +0900

    BUILD_LOCALLY: added sudo to make install

--- developer_toolbox/build_locally.sh
+++ developer_toolbox/build_locally.sh
@@ -2,4 +2,4 @@
 
 # WARNING: run this script from the project root folder, not from here!!
 
-cmake -DRETRODECK=on -DCMAKE_INSTALL_PREFIX=/app . && make && make install
+cmake -DRETRODECK=on -DCMAKE_INSTALL_PREFIX=/app . && make && sudo make install
