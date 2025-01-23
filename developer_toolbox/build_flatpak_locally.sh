818f6b55c068733f712d69c87e3341a3be762eb8
XargonWan <XargonWan@gmail.com>
2025-01-23 09:09:34 +0900

    Enhancing local build + cleanup

--- developer_toolbox/build_flatpak_locally.sh
+++ developer_toolbox/build_flatpak_locally.sh
@@ -2,4 +2,6 @@
 
 # WARNING: run this script from the project root folder, not from here!!
 
+rm -f CMakeCache.txt
+
 flatpak-builder --force-clean --user --install-deps-from=flathub --repo=esde-repo --install builddir net.retrodeck.es-de.yml
