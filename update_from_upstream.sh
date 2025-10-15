34cbf181f3ced0dbd80b1e5002b17831cc3139b7
XargonWan <XargonWan@gmail.com>
2025-10-15 14:59:08 +0900

    feat(tools): updated branch updaer tool

---
+++ update_from_upstream.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git fetch https://gitlab.com/es-de/emulationstation-de/ "master"  # Fetch the latest changes from the remote master branch
+git merge FETCH_HEAD  # Merge the fetched changes into your current branch
+echo -e "PLEASE CHECK IF ANYTHING IS CHANGED IN:\n-resources/systems/linux/es_find_rules.xml\n-resources/systems/linux/es_systems.xml"
