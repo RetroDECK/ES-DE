e7834e3bf0875f20c2ffa5b200304b173788c082
XargonWan <XargonWan@gmail.com>
2025-10-15 09:30:14 +0900

    fix(update_from_upstream): change fetch branch from stable-3.2 to master

--- update_from_upstream.sh
+++ update_from_upstream.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git fetch https://gitlab.com/es-de/emulationstation-de/ "stable-3.2"  # Fetch the latest changes from the remote master branch
+git fetch https://gitlab.com/es-de/emulationstation-de/ "master"  # Fetch the latest changes from the remote master branch
 git merge FETCH_HEAD  # Merge the fetched changes into your current branch
 echo -e "PLEASE CHECK IF ANYTHING IS CHANGED IN:\n-resources/systems/linux/es_find_rules.xml\n-resources/systems/linux/es_systems.xml"
