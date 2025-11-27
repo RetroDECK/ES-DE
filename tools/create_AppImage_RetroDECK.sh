f37d8e1ff897a9721c1329eb21c5197683ee9b3e
XargonWan <XargonWan@gmail.com>
2025-11-27 10:46:29 +0900

    fix(AppImage): add bundled certificates option to cmake command

--- tools/create_AppImage_RetroDECK.sh
+++ tools/create_AppImage_RetroDECK.sh
@@ -67,7 +67,7 @@ rm -rf ./AppDir
 mkdir AppDir
 
 rm -f CMakeCache.txt
-cmake -DAPPIMAGE_BUILD=on -DRETRODECK=on .
+cmake -DAPPIMAGE_BUILD=on -DRETRODECK=on -DBUNDLED_CERTS=on .
 make clean
 make -j${JOBS}
 make install DESTDIR=AppDir
